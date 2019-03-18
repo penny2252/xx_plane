720819dbb906f699fd16a04afda9ea1c11fea712
penny2252 <18643112252@163.com>
2019-03-19 00:55:47 +0800

    nothing

--- plane_main.py
+++ plane_main.py
@@ -12,16 +12,22 @@ class PlaneGame(object):
         self.clock = pygame.time.Clock()
         # 创建私有方法，精灵和精灵组创建
         self.__creat_sprites()
-        #设置定时器事件——创建敌机
-        pygame.time.set_timer(CREATE_ENEMY_EVENT,1000)
+        # 设置定时器事件——创建敌机
+        pygame.time.set_timer(CREATE_ENEMY_EVENT, 1000)
+        #设置发射子弹事件
+        pygame.time.set_timer(HERO_FIRE_EVENT,500)
 
     def __creat_sprites(self):
         # 创建背景精灵和精灵组
         bg1 = BackGround()
         bg2 = BackGround(True)
         self.back_group = pygame.sprite.Group(bg1, bg2)
-        #创建敌机精灵组
-        self.enemy_group=pygame.sprite.Group()
+        # 创建敌机精灵组
+        self.enemy_group = pygame.sprite.Group()
+        self.hero = Hero()
+        self.hero_group = pygame.sprite.Group(self.hero)
+        self.bullet_group=pygame.sprite.Group()
+
     def start_game(self):
         print('kaishi')
         while True:
@@ -30,6 +36,7 @@ class PlaneGame(object):
             # 事件监听
             self.__event_handler()
             # 碰撞检测
+
             self.__check_collide()
             # 更新绘制精灵
             self.__updat_sprites()
@@ -37,14 +44,35 @@ class PlaneGame(object):
             pygame.display.update()
 
     def __event_handler(self):
+
+
         for event in pygame.event.get():
             # 判断是否退出
             if event.type == pygame.QUIT:
                 PlaneGame.__game_over()
-            elif event.type==CREATE_ENEMY_EVENT:
-                #创建精灵，添加到精灵组
-                enemy=Enemy()
+            elif event.type == CREATE_ENEMY_EVENT:
+                # 创建精灵，添加到精灵组
+                enemy = Enemy()
                 self.enemy_group.add(enemy)
+            elif event.type==HERO_FIRE_EVENT:
+                self.hero.fire()
+
+            #键盘方向捕获方式1，只有按下才被捕获，然后触发移动
+            # elif event.type==pygame.KEYDOWN and event.key==pygame.K_RIGHT:
+            #     self.hero.rect.x+=5
+            # elif event.type == pygame.KEYDOWN and event.key == pygame.K_LEFT:
+            #     self.hero.rect.x -= 5
+        #使用键盘提供的方式捕获键盘按键的元组,如果按下元组为1,这种方式相当于连续按键
+        keys_pressed=pygame.key.get_pressed()
+        #判断元组中对应的按键索引值
+        if keys_pressed[pygame.K_RIGHT]:
+            self.hero.speed=2
+        elif keys_pressed[pygame.K_LEFT]:
+            self.hero.speed=-2
+        else:
+            self.hero.speed=0
+
+
 
     def __check_collide(self):
         pass
@@ -54,7 +82,10 @@ class PlaneGame(object):
         self.back_group.draw(self.screen)
         self.enemy_group.update()
         self.enemy_group.draw(self.screen)
-
+        self.hero_group.update()
+        self.hero_group.draw(self.screen)
+        self.hero.bullets.update()
+        self.hero.bullets.draw(self.screen)
 
     @staticmethod
     def __game_over():
--- plane_sprites.py
+++ plane_sprites.py
@@ -1,46 +1,108 @@
 import random
 import pygame
-#定义屏幕大小的常量,常量名所有字符大写
-SCREEN_RECT=pygame.Rect(0,0,480,700)
-#定义刷新帧数的常量
-FRAME_PER_SEC=60
-#创建敌机的定时器常量
-CREATE_ENEMY_EVENT=pygame.USEREVENT
+
+# 定义屏幕大小的常量,常量名所有字符大写
+SCREEN_RECT = pygame.Rect(0, 0, 480, 700)
+# 定义刷新帧数的常量
+FRAME_PER_SEC = 60
+# 创建敌机的定时器常量
+CREATE_ENEMY_EVENT = pygame.USEREVENT
+#发射子弹事件
+HERO_FIRE_EVENT=pygame.USEREVENT+1
+
 
 class GameSprite(pygame.sprite.Sprite):
-    '''飞机大战游戏精灵'''
+    """飞机大战游戏精灵"""
 
-    def __init__(self,image_name,speed=1):
-        #调用父类初始化方法
+    def __init__(self, image_name, speed=1):
+        # 调用父类初始化方法
         super().__init__()
-        #定义对象属性
-        self.image=pygame.image.load(image_name)
-        self.rect=self.image.get_rect()
-        self.speed=speed
+        # 定义对象属性
+        self.image = pygame.image.load(image_name)
+        self.rect = self.image.get_rect()
+        self.speed = speed
+
     def update(self):
-        #在屏幕的垂直方向移动
-        self.rect.y+=self.speed
+        # 在屏幕的垂直方向移动
+        self.rect.y += self.speed
+
+
 class BackGround(GameSprite):
-    '''背景精灵'''
-    def __init__(self,is_alt=False):
-        #调用父类方法实现精灵创建
+    """背景精灵"""
+
+    def __init__(self, is_alt=False):
+        # 调用父类方法实现精灵创建
         super().__init__('./feiji/background.png')
-        #判断是否交替图像，需要设置初始位置
+        # 判断是否交替图像，需要设置初始位置
         if is_alt:
-            self.rect.y=-self.rect.height
+            self.rect.y = -self.rect.height
+
     def update(self):
-        #调用父类方法
+        # 调用父类方法
         super().update()
-        #判断是否移除屏幕，如果移出，图像到屏幕上方
-        if self.rect.y>=SCREEN_RECT.height:
-            self.rect.y=-self.rect.height
+        # 判断是否移除屏幕，如果移出，图像到屏幕上方
+        if self.rect.y >= SCREEN_RECT.height:
+            self.rect.y = -self.rect.height
+
+
 class Enemy(GameSprite):
-    '''敌机精灵'''
-    def __init__(self,is_alt=False):
-        super().__init__('./feiji/enemy1.png')
-        self.rect.x=random.randint(0,480-self.rect.height)
-        self.speed=random.randint(1,2)
+    """敌机精灵"""
+
+    def __init__(self):
+        super().__init__('./feiji/enemy0.png')
+        # 敌机随机位置
+        max_x = SCREEN_RECT.width - self.rect.width
+        self.rect.x = random.randint(0, max_x)
+        self.rect.y = -self.rect.height
+        # bottom=y+self.rect.height
+        # self.rect.bottom=0
+        # 敌机随机速度
+        self.speed = random.randint(1, 2)
+
     def update(self):
         super().update()
-        if self.rect.y>SCREEN_RECT.height:
-            pass
+        if self.rect.y > SCREEN_RECT.height:
+            # kill可以将精灵从精灵组中销毁
+            self.kill()
+
+    def __del__(self):
+        pass
+
+
+class Hero(GameSprite):
+    """英雄精灵"""
+
+    def __init__(self):
+        super().__init__('./feiji/hero1.png', 0)
+        self.rect.centerx = SCREEN_RECT.centerx
+        self.rect.bottom = SCREEN_RECT.bottom - 120
+        #创建子弹精灵组
+        self.bullets=pygame.sprite.Group()
+    def update(self):
+        self.rect.x += self.speed
+        if self.rect.x < 0:
+            self.rect.x = 0
+        elif self.rect.right > SCREEN_RECT.right:
+            self.rect.right = SCREEN_RECT.right
+
+    def fire(self):
+        for i in (0,1,2):
+            #创建子弹精灵
+            bullet=Bullet()
+            #设置子弹位置
+            bullet.rect.bottom=self.rect.y-20*i
+            bullet.rect.centerx=self.rect.centerx
+            #添加到精灵组
+            self.bullets.add(bullet)
+
+
+class Bullet(GameSprite):
+
+    def __init__(self):
+        super().__init__('./feiji/bullet1.png',-2)
+    def update(self):
+        super().update()
+        if self.rect.bottom<0:
+            self.kill()
+    def __del__(self):
+        pass
